8de6cc3507384d7a8f1c4b4a5ac22253fc53c8ca
shubhanker <ankursaxena347@gmail.com>
2025-08-29 00:51:46 +0530

    wip

--- src/app/components/VolunteerSection.tsx
+++ src/app/components/VolunteerSection.tsx
@@ -17,15 +17,15 @@ const VolunteerSection = () => {
       />
       <Container>
         <div className="absolute inset-0 flex items-center justify-center px-4 md:px-0 lg:px-8 xl:px-12">
-          <div className="rounded-2xl lg:rounded-3xl xl:rounded-[32px] max-w-4xl lg:max-w-5xl xl:max-w-6xl w-full">
-            <h2 className="text-4xl md:text-[130px] lg:text-[150px] xl:text-[170px] text-center font-frank text-[#2D2B42] capitalize">
+          <div className="rounded-2xl lg:rounded-3xl 2xl:rounded-[32px] max-w-4xl lg:max-w-5xl xl:max-w-6xl w-full">
+            <h2 className="text-4xl md:text-[130px] lg:text-[130px] 2xl:text-[170px] text-center font-frank text-[#2D2B42] capitalize">
               Volunteers
             </h2>
-            <p className="text-white text-lg leading-8 md:text-2xl lg:text-3xl xl:text-4xl text-center font-inter mt-4 md:mt-10 lg:mt-12 xl:mt-16 px-4 md:px-0 lg:px-8 xl:px-12 lg:leading-relaxed xl:leading-relaxed">
+            <p className="text-white text-lg leading-8 md:text-2xl lg:text-2xl 2xl:text-4xl text-center font-inter mt-4 md:mt-10 lg:mt-12 xl:mt-16 px-4 md:px-0 lg:px-8 xl:px-12 lg:leading-relaxed xl:leading-relaxed">
             We can always use a few more helping hands. If you&apos;re interested in being added to our volunteer roster to help out, please fill out the form.
             </p>
             <div className="mt-6 md:pb-0 pb-10 md:mt-8 lg:mt-12 xl:mt-16 flex justify-center">
-              <div className="w-48 md:w-200 lg:w-[250px] xl:w-[300px]">
+              <div className="w-48 md:w-200 lg:w-[200px] 2xl:w-[300px]">
                 <button 
                   onClick={handleVolunteerOnboarding}>
                 <Image 
--- src/app/components/amazonSection.tsx
+++ src/app/components/amazonSection.tsx
@@ -31,7 +31,7 @@ function AmazonSection() {
             </div>
           </div> */}
 
-          <div className="flex flex-col space-y-6 lg:space-y-8 xl:space-y-10 text-start">
+          <div className="flex flex-col space-y-6 lg:space-y-8 2xl:space-y-10 text-start">
             {/* Making Donations Section */}
             {/* <div className="border mt-10 rounded-2xl border-[#2D2B42] p-4 md:p-8">
               <div className="font-frank capitalize text-3xl md:text-5xl text-[#2D2B42]">
@@ -65,31 +65,31 @@ function AmazonSection() {
                     alt="Donate via Amazon"
                     width={270}
                     height={80}
-                    className="md:h-auto md:w-auto w-[200px] lg:w-[320px] xl:w-[400px] cursor-pointer hover:scale-105 transition-transform duration-300"
+                    className="md:h-auto md:w-auto w-[200px] lg:w-[320px] 2xl:w-[400px] cursor-pointer hover:scale-105 transition-transform duration-300"
                   />
                 </div>
-                <div className="mt-4 lg:mt-6 xl:mt-8 text-center font-frank capitalize text-3xl md:text-5xl lg:text-6xl 2xl:text-8xl leading-tight tracking-wide text-[#2D2B42]">
+                <div className="mt-4 lg:mt-6 2xl:mt-8 text-center font-frank capitalize text-3xl md:text-5xl lg:text-6xl 2xl:text-8xl leading-tight tracking-wide text-[#2D2B42]">
                   <p>OR</p>
                 </div>
               </div>
               <div className="flex justify-center">
-                <div className="w-full max-w-[800px] lg:max-w-[1000px] xl:max-w-[1200px]">
+                <div className="w-full max-w-[800px] lg:max-w-[1000px] 2xl:max-w-[1200px]">
                   {/* Label text */}
                   <label className="block text-[#2D2B42] text-base md:text-lg lg:text-xl 2xl:text-2xl font-medium mb-2 lg:mb-4 xl:mb-6 text-start font-frank leading-tight tracking-wider">
                     See if there&apos;s a donation location near you.
                   </label>
 
-                  <div className="flex mt-2 lg:mt-4 xl:mt-6 text-[#2D2B42]">
+                  <div className="flex mt-2 lg:mt-4 2xl:mt-6 text-[#2D2B42]">
                     <input
                       type="text"
                       placeholder="ZipCode"
-                      className="px-4 py-3 lg:px-6 lg:py-4 xl:px-8 xl:py-5 w-full border text-[#2D2B42] border-gray-400 rounded-l-2xl lg:rounded-l-3xl xl:rounded-l-[32px] text-base md:text-lg lg:text-xl xl:text-2xl bg-transparent outline-none"
+                      className="px-4 py-3 lg:px-6 lg:py-4 2xl:px-8 2xl:py-5 w-full border text-[#2D2B42] border-gray-400 rounded-l-2xl lg:rounded-l-3xl 2xl:rounded-l-[32px] text-base md:text-lg lg:text-xl 2xl:text-2xl bg-transparent outline-none"
                     />
-                    <button className="px-6 py-3 lg:px-8 lg:py-4 xl:px-10 xl:py-5 bg-black text-white text-base md:text-lg lg:text-xl xl:text-2xl font-bold rounded-r-2xl lg:rounded-r-3xl xl:rounded-r-[32px]">
+                    <button className="px-6 py-3 lg:px-8 lg:py-4 2xl:px-10 2xl:py-5 bg-black text-white text-base md:text-lg lg:text-xl 2xl:text-2xl font-bold rounded-r-2xl lg:rounded-r-3xl 2xl:rounded-r-[32px]">
                       Go
                     </button>
                   </div>
-                  <p className="text-start font-light text-sm lg:text-base xl:text-lg mt-2 lg:mt-4 xl:mt-6 italic text-gray-500">
+                  <p className="text-start font-light text-sm lg:text-sm 2xl:text-lg mt-2 lg:mt-4 xl:mt-6 italic text-gray-500">
                     If you don&apos;t see any results near you, you can still
                     donate to Foster Toys through Amazon. We&apos;re a small but
                     growing team, so we really appreciate your patience as we
@@ -98,20 +98,20 @@ function AmazonSection() {
                 </div>
               </div>
 
-              <div className="font-inter text-sm md:text-lg lg:text-xl xl:text-2xl text-[#2D2B42] italic">
+              <div className="font-inter text-sm md:text-lg lg:text-lg 2xl:text-2xl text-[#2D2B42] italic">
                 Found {agencies.length} results:
               </div>
 
               {/* Agency Grid */}
-              <div className="md:px-20 lg:px-32 xl:px-40">
+              <div className="md:px-20 lg:px-20 2xl:px-40">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-20 lg:gap-24 xl:gap-32 font-inter text-start">
                   {agencies.map((agency, index) => (
                     <div key={index} className="flex flex-col space-y-4 lg:space-y-6 xl:space-y-8">
-                      <div className="flex flex-col space-y-2 lg:space-y-3 xl:space-y-4">
-                        <div className="text-[#2D2B42] font-semibold text-base md:text-xl lg:text-2xl xl:text-3xl">
+                      <div className="flex flex-col space-y-2 lg:space-y-3 2xl:space-y-4">
+                        <div className="text-[#2D2B42] font-semibold text-base md:text-xl lg:text-xl 2xl:text-3xl">
                           {agency.name}
                         </div>
-                        <div className="text-[#2D2B42] text-sm md:text-lg lg:text-xl xl:text-2xl w-1/2 lg:w-2/3 xl:w-3/4 leading-8 lg:leading-relaxed xl:leading-relaxed font-light">
+                        <div className="text-[#2D2B42] text-sm md:text-lg lg:text-xl 2xl:text-2xl w-1/2 lg:w-2/3 xl:w-3/4 leading-8 lg:leading-relaxed xl:leading-relaxed font-light">
                           Shipping address: <br />
                           {agency.address}
                         </div>
