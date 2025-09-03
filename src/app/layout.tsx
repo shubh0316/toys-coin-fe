ae33bee81ec829dc101c31adc167637577873bbe
shubhanker <ankursaxena347@gmail.com>
2025-09-03 23:00:12 +0530

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
-  title: "Toys Coin",
+  title: "Foster Toys",
   description: "Dedicated to providing toys of comfort to over 400,000 kids entering into foster care.",
 };
 
