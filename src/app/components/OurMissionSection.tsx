99d93423df19216d4284c20dfa2e6fc68221f6c4
shubhanker <ankursaxena347@gmail.com>
2025-11-05 01:57:27 +0530

    wip: ui-done

--- src/app/components/OurMissionSection.tsx
+++ src/app/components/OurMissionSection.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 import mission from '@/assets/teddy2.png'
-import OurMission from '@/assets/OurMission.png'
+import OurMission from '@/assets/ourMission.png'
 
 
 function OurMissionSection() {
